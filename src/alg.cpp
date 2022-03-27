b16f61d4351751ce7a83fc218ebe984a64e67c34
AstariTTos <63050335+AstariTTos@users.noreply.github.com>
2022-03-27 17:49:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,24 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+int countPairs1(int* arr, int len, int val) {
+    int count = 0;
+        for (int c = 0; c < len-1; c++) {
+        for (int d = d + 1; d < len; d++) {
+           if (arr[c] + arr[d] == val) {
+                                count++;
+            }
+       }
+   }
+             return count;
+                      return 0;
+                }
+                int countPairs2(int* arr, int len, int val) {
+                              int count = 0;
+
+                      for (int c = 0; c < len-1; c++) {
+                           if(arr[c] > val) { break; }
+             for (int d = len-1; d > c; d--) {
+           if (arr[d] > val) continue;
+          if (arr[d] > val) { continue; }
+        if (arr[c] + arr[d] == val) {
+          count++;
+     }
